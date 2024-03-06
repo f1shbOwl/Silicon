fc69ee38bb650b8e9fd4c596125ad0c165fefbfe
Björn Lager Dalbratt <bjornlager@gmail.com>
2024-03-06 12:28:04 +0100

    signup init

---
+++ Presentation.MVC/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.MVC.Controllers;
+
+public class AuthController : Controller
+{
+    public IActionResult Index()
+    {
+        ViewData["Title"] = "Profile";
+        return View();
+    }
+
+    public IActionResult SignIn()
+    {
+        ViewData["Title"] = "Sign in";
+        return View();
+    }
+
+    [Route("/signup")]
+    public IActionResult SignUp()
+    {
+        return View();
+    }
+
+    public new IActionResult SignOut()
+    {
+        return RedirectToAction("Index", "Home");
+    }
+}
---
+++ Presentation.MVC/Models/Views/HomeIndexViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Presentation.MVC.Models.Sections;
+
+namespace Presentation.MVC.Models.Views;
+
+public class HomeIndexViewModel
+{
+    public string Title { get; set; } = "Ultimate Task Management Assistant";
+    public ShowcaseViewModel Showcase { get; set; } = new ShowcaseViewModel
+    {
+        Id = "overview",
+        ShowcaseImage = new() { ImageUrl = "images/showcase-taskmaster.svg", AltText = "Taskmanagement Assistant" },
+        Title = "Task Management Assistant You Gonna Love",
+        Text = "We offer you a new generation of task management system. Plan, manage & track all your tasks in one flexible tool.",
+        Link = new() { ControllerName = "Downloads", ActionName = "Index", Text = "Get started for free" },
+        BrandsText = "Largest companies use our tool to work efficiently",
+        Brands =
+                [
+                    new() { ImageUrl = "images/brands/brand_1.svg", AltText = "Brand Name 1" },
+                    new() { ImageUrl = "images/brands/brand_2.svg", AltText = "Brand Name 2" },
+                    new() { ImageUrl = "images/brands/brand_3.svg", AltText = "Brand Name 3" },
+                    new() { ImageUrl = "images/brands/brand_4.svg", AltText = "Brand Name 4" },
+                ],
+    };
+ }

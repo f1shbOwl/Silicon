a5d789760fba1d6345dc5424cc9e11168a104188
Björn Lager Dalbratt <bjornlager@gmail.com>
2024-03-14 10:40:26 +0100

    signin done.

---
+++ Presentation.MVC/ViewModels/Sections/SignInViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Presentation.MVC.Models;
+
+namespace Presentation.MVC.ViewModels.Sections;
+
+public class SignInViewModel
+{
+    public string Title { get; set; } = "Sign in";
+    public SignInModel Form { get; set; } = new SignInModel();
+    public string? ErrorMessage { get; set; }
+}

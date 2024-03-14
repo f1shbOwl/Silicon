2b9e0aea74e4576973d00160a3834e9720201564
Björn Lager Dalbratt <bjornlager@gmail.com>
2024-03-14 10:53:57 +0100

    Account init

--- Presentation.MVC/Controllers/AccountController.cs
+++ Presentation.MVC/Controllers/AccountController.cs
@@ -4,5 +4,10 @@ namespace Presentation.MVC.Controllers;
 
 public class AccountController : Controller
 {
+   //private readonly AccountService _accountService;
 
+   // public AccountController(AccountService accountService)
+   // {
+   //     _accountService = accountService;
+   // }
 }

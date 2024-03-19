f77eb464b40a7c2a74b9233d45bd4ec961c0affe
Björn Lager Dalbratt <bjornlager@gmail.com>
2024-03-19 11:47:42 +0100

    validering av signup klar.

--- Presentation.MVC/Models/SignUpModel.cs
+++ Presentation.MVC/Models/SignUpModel.cs
@@ -5,35 +5,42 @@ namespace Presentation.MVC.Models;
 
 public class SignUpModel
 {
+    [DataType(DataType.Text)]
     [Display(Name = "First name", Prompt = "Enter your first name", Order = 0)]
-    [Required(ErrorMessage = "First name is required")]
+    [Required(ErrorMessage = "Enter your first name")]
+    [MinLength(2, ErrorMessage = "Enter your first name")]
     public string FirstName { get; set; } = null!;
 
+
+    [DataType(DataType.Text)]
     [Display(Name = "Last name", Prompt = "Enter your last name", Order = 1)]
-    [Required(ErrorMessage = "Last name is required")]
+    [Required(ErrorMessage = "Enter your last name")]
+    [MinLength(2, ErrorMessage = "Enter your last name")]
     public string LastName { get; set; } = null!;
+
 
     [Display(Name = "Email address", Prompt = "Enter your email address", Order = 2)]
     [DataType(DataType.EmailAddress)]
-    [Required(ErrorMessage = "Email is required")]
-    [RegularExpression("^\\S+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", ErrorMessage = "Invalid email address")]
+    [Required(ErrorMessage = "Email a valid email address")]
+    [RegularExpression("^(([^<>()\\[\\]\\\\.,;:\\s@\"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@\"]+)*)|(\".+\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$", ErrorMessage = "Email a valid email address")]
     public string Email { get; set; } = null!;
+
 
     [Display(Name = "Password", Prompt = "Enter your password", Order = 3)]
     [DataType(DataType.Password)]
-    [Required(ErrorMessage = "Password is required")]
-    [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^a-zA-Z\\d\\s]).{8,}$", ErrorMessage = "A minimum 8 characters, password must contain a combination of an uppercase and lowercase letter, a number and a special character")]
+    [Required(ErrorMessage = "Enter a valid password")]
+    [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", ErrorMessage = "Enter a valid password")]
     public string Password { get; set; } = null!;
 
 
     [Display(Name = "Confirm password", Prompt = "Confirm your passwrod", Order = 4)]
     [DataType(DataType.Password)]
     [Required(ErrorMessage = "Password must be confirmed")]
-    [Compare(nameof(Password), ErrorMessage = "Password does not match")]
+    [Compare(nameof(Password), ErrorMessage = "Password must be confirmed")]
     public string ConfirmPassword { get; set; } = null!;
 
 
-    [Display(Name = "I agree to Terms & Conditions.", Order = 5)]
+    [Display(Name = "I agree to Terms & Conditions ", Order = 5)]
     [CheckboxRequired(ErrorMessage = "You must accept terms and conditions to proceed")]
     public bool TermsAndCondition { get; set; } = false;
 
